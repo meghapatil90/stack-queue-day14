d74c523a2e16d49f0b663cd7eb9bc5a83aee7625
megha patil <megharpatil2000@gmail.com>
2022-10-21 22:46:38 +0530

    peek pop

--- stackAndQueue/Stack.java
+++ stackAndQueue/Stack.java
@@ -4,26 +4,53 @@ import java.util.LinkedList;
 
 public class Stack<T> {
 
-	private LinkedList<T> list;
+	 private LinkedList<T> list;
 
-	Stack() {
-		list = new LinkedList<T>();
-	}
+	    Stack() {
+	        list = new LinkedList<T>();
+	    }
 
-	void push(T item) {
-		list.add(item);
-	}
+	    void push(T item) {
+	        list.add(item);
+	    }
 
-	void show() {
-		System.out.println(list);
-	}
+	    T peek() {
+	        return list.getLast();
+	    }
 
-	public static void main(String[] args) {
-		Stack<Integer> nums = new Stack<>();
-		nums.push(70);
-		nums.push(30);
-		nums.push(56);
+	    T pop() {
+	        return list.removeLast();
+	    }
 
-		nums.show();
-	}
-}
+	    int size() {
+	        return list.size();
+	    }
+
+	    boolean isEmpty() {
+	        return list.isEmpty();
+	    }
+
+	    void show() {
+	        System.out.println(list);
+	    }
+
+	    public static void main(String[] args) {
+	        Stack<Integer> nums = new Stack<>();
+	        nums.push(70);
+	        nums.push(30);
+	        nums.push(56);
+
+	        System.out.println("Elements in stack:");
+	        nums.show();
+
+	        int size = nums.size();
+	        System.out.println("Size of stack: " + size);
+
+	        while (! nums.isEmpty()) {
+	            System.out.println(nums.peek());
+	            nums.pop();
+	        }
+
+	        System.out.println("Elements in stack after popping:");
+	        nums.show();
+	    }}

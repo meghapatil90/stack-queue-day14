0b61f57a60209a280fafc211b1a195b09df7b9e3
megha patil <megharpatil2000@gmail.com>
2022-10-21 22:54:47 +0530

    Added queue

--- stackAndQueue/Stack.java
+++ stackAndQueue/Stack.java
@@ -4,53 +4,54 @@ import java.util.LinkedList;
 
 public class Stack<T> {
 
-	 private LinkedList<T> list;
+	private LinkedList<T> list;
 
-	    Stack() {
-	        list = new LinkedList<T>();
-	    }
+	Stack() {
+		list = new LinkedList<T>();
+	}
 
-	    void push(T item) {
-	        list.add(item);
-	    }
+	void push(T item) {
+		list.add(item);
+	}
 
-	    T peek() {
-	        return list.getLast();
-	    }
+	T peek() {
+		return list.getLast();
+	}
 
-	    T pop() {
-	        return list.removeLast();
-	    }
+	T pop() {
+		return list.removeLast();
+	}
 
-	    int size() {
-	        return list.size();
-	    }
+	int size() {
+		return list.size();
+	}
 
-	    boolean isEmpty() {
-	        return list.isEmpty();
-	    }
+	boolean isEmpty() {
+		return list.isEmpty();
+	}
 
-	    void show() {
-	        System.out.println(list);
-	    }
+	void show() {
+		System.out.println(list);
+	}
 
-	    public static void main(String[] args) {
-	        Stack<Integer> nums = new Stack<>();
-	        nums.push(70);
-	        nums.push(30);
-	        nums.push(56);
+	public static void main(String[] args) {
+		Stack<Integer> nums = new Stack<>();
+		nums.push(70);
+		nums.push(30);
+		nums.push(56);
 
-	        System.out.println("Elements in stack:");
-	        nums.show();
+		System.out.println("Elements in stack:");
+		nums.show();
 
-	        int size = nums.size();
-	        System.out.println("Size of stack: " + size);
+		int size = nums.size();
+		System.out.println("Size of stack: " + size);
 
-	        while (! nums.isEmpty()) {
-	            System.out.println(nums.peek());
-	            nums.pop();
-	        }
+		while (!nums.isEmpty()) {
+			System.out.println(nums.peek());
+			nums.pop();
+		}
 
-	        System.out.println("Elements in stack after popping:");
-	        nums.show();
-	    }}
+		System.out.println("Elements in stack after popping:");
+		nums.show();
+	}
+}
